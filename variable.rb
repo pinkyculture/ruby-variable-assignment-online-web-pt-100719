9bca09da81fe51f52d9194314f45be751a3d9049
interactive-cache-8375 <randomblue1@yahoo.com>
2019-10-15 18:15:06 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,4 +1,11 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+<<<<<<< HEAD
 
 greeting = "Hello World"
+=======
+def greeting
+greeting = "Hello World"
+puts greeting 
+end 
+>>>>>>> 85421326f3728e40e269e2e4f0182786411739f1
